3262908ce0e6423bc2a3052e607b37f73d7070a4
Jason Robock <jrobock@ualberta.ca>
2020-11-20 15:00:29 -0700

    added scoring for tree heuristic

--- connect4.py
+++ connect4.py
@@ -10,11 +10,19 @@ class Game_board:
             print('\n')
         print("1\t2\t3\t4\t5\t6\t7")
     def move(self, position, player):
-        for row in self.pos:
-            if row[position] == "*":
-                row[position] = player
+        for row in range(len(self.pos)):
+        #for row in self.pos:
+            if self.pos[row][position] == "*":
+                self.pos[row][position] = player
+                self.moves.append((row,position))
                 return False
         return True
+    
+    def remove_move(self):
+        move = self.moves.pop()
+        self.pos[move[0]][move[1]] = '*'
+        return
+
     def board_full(self):
         total = 0
         size = len(self.pos) * len(self.pos[0])
@@ -25,6 +33,13 @@ class Game_board:
         if total == size:
             return True
         return False
+    
+    def is_empty(self):
+        for row in self.pos:
+            for column in row:
+                if column != '*':
+                    return False
+        return True
 
     #i = columns j = rows?
     def check_win(self, symbol):
@@ -52,11 +67,70 @@ class Game_board:
                         if self.pos[i][j+1] == symbol and self.pos[i][j+2] == symbol and self.pos[i][j+3] == symbol:
                             return True
         return False
+    
+    def check_score(self, symbol, first=0):
+        if first == 0:
+            other_score = self.check_score("B" if symbol == "R" else "R", 1)
+        score = 0
+        #counted = []
+        for i in range(6):
+            for j in range(7):
+                up_checked = 0
+                if self.pos[i][j] == symbol:
+                    if (j - 3) >= 0:
+                        if (i + 3) <= 5: #5? maybe? check for off by one
+                            #check stright up and diagonal up 
+                            if self.pos[i+1][j] == symbol and self.pos[i+2][j] == symbol and self.pos[i+3][j] == '*' and up_checked == 0:
+                                score += 50
+                                up_checked = 1
+                            elif self.pos[i+1][j] == symbol and self.pos[i+2][j] == '*' and up_checked == 0:
+                                if i > 0:
+                                    if self.pos[i-1][j] == symbol:
+                                        pass
+                                    else:
+                                        score += 10
+                                        up_checked = 1
+                                else:
+                                    score += 10
+                                    up_checked = 1
+                            if self.pos[i+1][j-1] == symbol and self.pos[i+2][j-2] == symbol and self.pos[i+3][j-3] == '*':
+                                score += 50   
+                            elif self.pos[i+1][j-1] == symbol and self.pos[i+2][j-2] == '*':
+                                score += 10
+                    if (j + 3) <= 6: #6? maybe?
+                        if(i + 3) <= 5:
+                            #check up and up right (need to figure out how not to double add up)
+                            if self.pos[i+1][j] == symbol and self.pos[i+2][j] == symbol and self.pos[i+3][j] == '*' and up_checked == 0:
+                                score += 50
+                                up_checked = 1
+                            elif self.pos[i+1][j] == symbol and self.pos[i+2][j] == '*' and up_checked == 0:
+                                if i > 0:
+                                    if self.pos[i-1][j] == symbol:
+                                        pass
+                                    else:
+                                        score += 10
+                                        up_checked = 1
+                                else:
+                                    score += 10
+                                    up_checked = 1
+
+                            #up right
+                            if self.pos[i+1][j+1] == symbol and self.pos[i+2][j+2] == symbol and self.pos[i+3][j+3] == '*':
+                                score += 50
+                            elif self.pos[i+1][j+1] == symbol and self.pos[i+2][j+2] == '*':
+                                score += 10
+                        #check right
+                        if self.pos[i][j+1] == symbol and self.pos[i][j+2] == symbol and self.pos[i][j+3] == '*':
+                            score += 50
+        if first == 1:
+            return score
+        else:
+            return score - other_score
 
     def print_help(self):
         print("'h' for help")
         print("Red goes first player enters 1-7 to pick which slot to play in")
-        print("B to go back a move")
+        print("b to go back a move")
         print("exit to exit")
 
     def two_play_game(self):
@@ -72,7 +146,12 @@ class Game_board:
             elif move == "exit":
                 exit()
             elif move == "b":
-                pass # need to do this still lolololol
+                if board.is_empty():
+                    print("Board is empty, please make a move first.\n")
+                else:
+                    board.remove_move()
+            elif move == "s":
+                print(board.check_score("R"))
             else:
                 if player == 0:
                     while(self.move(int(move)-1, 'R')):
